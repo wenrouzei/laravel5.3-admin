fe9589de64468abbcf80980baea7fa309e9a2579
wenrouzei <wenrouzei@163.com>
2016-12-07 17:39:24 +0800

    alter restful资源控制器路由限制更新

--- routes/admin.php
+++ routes/admin.php
@@ -49,13 +49,15 @@ Route::group(['middleware' => ['auth:admin', 'menu', 'authAdmin']], function () 
                 'create' => 'admin.permission.create', 
                 'store' => 'admin.permission.create',
                 'destroy' => 'admin.permission.destroy'
-            ]
+            ],
+            'except' => ['show']//限制访问路由
         ]
     );
 
 
     //角色管理路由
     Route::match(['get', 'post'], 'role/index',['as' => 'admin.role.index', 'uses' => 'RoleController@index']);//post为ajax请求用到
+    //RESTful 资源控制器
     Route::resource('role', 'RoleController',
         ['names' => 
             [
@@ -64,13 +66,15 @@ Route::group(['middleware' => ['auth:admin', 'menu', 'authAdmin']], function () 
                 'create' => 'admin.role.create', 
                 'store' => 'admin.role.create',
                 'destroy' => 'admin.role.destroy'
-            ]
+            ],
+            'except' => ['show']//限制访问路由
         ]
     );
 
 
     //用户管理路由
     Route::match(['get', 'post'], 'user/index',['as' => 'admin.user.index', 'uses' => 'UserController@index']);//post为ajax请求用到
+    //RESTful 资源控制器
     Route::resource('user', 'UserController',
         ['names' => 
             [
@@ -79,7 +83,8 @@ Route::group(['middleware' => ['auth:admin', 'menu', 'authAdmin']], function () 
                 'create' => 'admin.user.create', 
                 'store' => 'admin.user.create',
                 'destroy' => 'admin.user.destroy'
-            ]
+            ],
+         'except' => ['show']//限制访问路由
         ]
     );
 
